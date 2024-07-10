87ae1a53b979251069f7f1fe044d1638daecf418
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-07-10 14:19:33 +0600

    added Nav component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode, ComponentProps } from "react";
+
+export function Nav({ children }: { children: ReactNode }) {
+  return (
+    <nav className="bg-primary text-primary-foreground flex justify-center px-4">
+      {children}
+    </nav>
+  );
+}
+
+export function NavLink(props: ComponentProps<typeof Link>) {
+  const pathname = usePathname();
+  return (
+    <Link
+      {...props}
+      className={cn(
+        "p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground",
+        pathname === props.href && "bg-background text-foreground"
+      )}
+    />
+  );
+}
